fddaaf36e11f2cd396930275e4a359a8053adcce
Reina <reina.khasanova@iaau.edu.kg>
2021-07-09 16:52:45 +0600

    recyclerview

---
+++ app/src/main/java/com/example/myapplication/CustomRecyclerAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomRecyclerAdapter(private val names: List<String>) :
+    RecyclerView.Adapter<CustomRecyclerAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var largeTextView: TextView? = null
+        var smallTextView: TextView? = null
+
+        init {
+            largeTextView = itemView.findViewById(R.id.title_text)
+            smallTextView = itemView.findViewById(R.id.phone_text)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.list_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.largeTextView?.text = names[position]
+        holder.smallTextView?.text = "кот"
+    }
+
+    override fun getItemCount() = names.size
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,56 +1,26 @@
 package com.example.myapplication
 
-import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.BaseAdapter
-import android.widget.ListView
-import android.widget.TextView
+
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var listView: ListView
-    var arrayList: ArrayList<MyData> = ArrayList()
-    var adapter: MyAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
-        title = "KotlinApp"
-        listView = findViewById(R.id.listview)
-        arrayList.add(MyData(1, " Mashu", "987576443"))
-        arrayList.add(MyData(2, " Azhar", "8787576768"))
-        arrayList.add(MyData(3, " Niyaz", "65757657657"))
-        adapter = MyAdapter(this, arrayList)
-        listView.adapter = adapter
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = CustomRecyclerAdapter(fillList())
+    }
+
+    private fun fillList(): List<String> {
+        val data = mutableListOf<String>()
+        (0..30).forEach { i -> data.add(" ${i} element") }
+        return data
     }
 }
-//Class MyAdapter
-class MyAdapter(private val context: Context, private val arrayList: java.util.ArrayList<MyData>) : BaseAdapter() {
-    private lateinit var serialNum: TextView
-    private lateinit var name: TextView
-    private lateinit var contactNum: TextView
-    override fun getCount(): Int {
-        return arrayList.size
-    }
-    override fun getItem(position: Int): Any {
-        return position
-    }
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
-    }
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
-        var convertView = convertView
-        convertView = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
-        serialNum = convertView.findViewById(R.id.number_text)
-        name = convertView.findViewById(R.id.title_text)
-        contactNum = convertView.findViewById(R.id.phone_text)
-        serialNum.text = " " + arrayList[position].num
-        name.text = arrayList[position].name
-        contactNum.text = arrayList[position].mobileNumber
-        return convertView
-    }
-}
-class MyData(var num: Int, var name: String, var mobileNumber: String)
