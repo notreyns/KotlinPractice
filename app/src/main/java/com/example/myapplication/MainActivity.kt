289da156ead2116017c0b73a34360c93b2db850b
Reina <reina.khasanova@iaau.edu.kg>
2021-07-09 15:14:11 +0600

    createFromResource, strings array

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var array= arrayOf("Cat", "Dog", "Fish")
-
-        val adapter= ArrayAdapter(this, R.layout.list_item,R.id.title_text, array)
+        val array= resources.getStringArray(R.array.dayofweek)
+        val adapter= ArrayAdapter(this,R.layout.list_item, R.id.title_text, array)
         val listView: ListView = findViewById(R.id.listview)
         listView.adapter= adapter
     }
